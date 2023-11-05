0981693d7ecc570073ea17d4d37d47a594b1d2bc
junsongxiao <junsong.xiao@lincolnuni.ac.nz>
2023-11-05 18:18:09 +1300

    updated database

--- cinema_schema.sql
+++ cinema_schema.sql
@@ -1,16 +1,5 @@
-select * from users;
-select * from movies;
-select * from bookings;
-select * from cinemahalls;
-select * from screenings;
-select * from cinemahallseats;
-select * from payments;
-select * from coupons;
-select * from notifications;
-select * from screenings;
-select * from booked_seats;
 
-
+SELECT * FROM MOVIES;
 
 -- Create cinema schema
 CREATE SCHEMA IF NOT EXISTS `cinema` DEFAULT CHARACTER SET utf8;
@@ -135,7 +124,7 @@ INSERT INTO Users (Name, Address, Email, Phone, Username, Password, Type)
 VALUES
 ('Admin admin', '123 xxx Street, NZ', 'admin@email.com', '+11234567890', 'admin', 'admin', 'Admin'),
 ('staff staff', '123 xxx Street, NZ', 'staff@email.com', '+11234567891', 'staff', 'staff', 'FrontDeskStaff'),
-('Customer Customer', '123 xxx Street, NZ', 'admin1@email.com', '+11234567890', 'customer', 'customer', 'Custoomer');
+('Customer Customer', '123 xxx Street, NZ', 'admin1@email.com', '+11234567890', 'customer', 'customer', 'Customer');
 
 
 -- CinemaHalls Table
@@ -261,18 +250,18 @@ VALUES
 
   
 -- Movies Table
-INSERT INTO Movies (Title, Description, DurationMins, Language, ReleaseDate, Country, Genre)
+INSERT INTO Movies (Title, Description, DurationMins, Language, ReleaseDate, Director,Country, Genre)
 VALUES 
-('Avatar', 'A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.', 162, 'English', '2009-12-18', 'USA', 'Sci-Fi'),
-('Titanic', 'A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.', 195, 'English', '1997-12-19', 'USA', 'Romance'),
-('Interstellar', 'A group of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.', 169, 'English', '2014-11-07', 'USA', 'Sci-Fi'),
-('Inception', 'A thief who enters the dreams of others to steal secrets from their subconscious is tasked with planting an idea into the mind of a CEO.', 148, 'English', '2010-07-16', 'USA', 'Sci-Fi'),
-('The Shawshank Redemption', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 142, 'English', '1994-09-22', 'USA', 'Drama'),
-('The Godfather', 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.', 175, 'English', '1972-03-24', 'USA', 'Crime'),
-('Pulp Fiction', 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.', 154, 'English', '1994-10-14', 'USA', 'Crime'),
-('The Dark Knight', 'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.', 152, 'English', '2008-07-18', 'USA', 'Action'),
-('The Matrix', 'A computer programmer discovers that reality as he knows it is a simulation created by machines to subjugate humanity.', 136, 'English', '1999-03-31', 'USA', 'Sci-Fi'),
-('Forrest Gump', 'The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other historical events unfold through the perspective of an Alabama man with an IQ of 75.', 142, 'English', '1994-07-06', 'USA', 'Drama');
+('Avatar', 'A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.', 162, 'English', '2009-12-18', 'James Cameron','USA', 'Sci-Fi'),
+('Titanic', 'A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.', 195, 'English', '1997-12-19', 'James Cameron','USA', 'Romance'),
+('Interstellar', 'A group of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.', 169, 'English', '2014-11-07','James Cameron', 'USA', 'Sci-Fi'),
+('Inception', 'A thief who enters the dreams of others to steal secrets from their subconscious is tasked with planting an idea into the mind of a CEO.', 148, 'English', '2010-07-16', 'James Cameron','USA', 'Sci-Fi'),
+('The Shawshank Redemption', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 142, 'English', '1994-09-22', 'James Cameron','USA', 'Drama'),
+('The Godfather', 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.', 175, 'English', '1972-03-24', 'James Cameron','USA', 'Crime'),
+('Pulp Fiction', 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.', 154, 'English', '1994-10-14', 'James Cameron','USA', 'Crime'),
+('The Dark Knight', 'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.', 152, 'English', '2008-07-18','James Cameron', 'USA', 'Action'),
+('The Matrix', 'A computer programmer discovers that reality as he knows it is a simulation created by machines to subjugate humanity.', 136, 'English', '1999-03-31','James Cameron', 'USA', 'Sci-Fi'),
+('Forrest Gump', 'The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other historical events unfold through the perspective of an Alabama man with an IQ of 75.', 142, 'English', '1994-07-06', 'James Cameron','USA', 'Drama');
 
 
 -- Screenings Table
@@ -312,16 +301,11 @@ INSERT INTO Payments (Amount, CreatedOn, CouponID) VALUES
 
 -- booking table
 INSERT INTO Bookings (UserID, MovieID, Date, ScreeningID, PaymentID, StaffID, Booking_Status) VALUES
-(4, 4, '2023-06-07', 7, 6, NULL, 'Confirmed'),
-(5, 5, '2023-06-09', 9, 7, 6, 'Pending'),
-(6, 1, '2023-06-02', 2, 8, 7, 'Confirmed'),
-(7, 2, '2023-06-04', 4, 9, 8, 'Pending'),
-(8, 3, '2023-06-06', 6, 10, NULL, 'Refunded'),
-(9, 4, '2023-06-08', 8, 5, 6, 'Pending'),
-(10, 5, '2023-06-10', 10, 4, 7, 'Confirmed'),
-(1, 2, '2023-06-03', 3, 3, 8, 'Pending'),
-(2, 3, '2023-06-05', 5, 2, NULL, 'Confirmed'),
-(3, 4, '2023-06-07', 7, 1, 6, 'Pending');
+(3, 4, '2023-12-07', 7, 6, NULL, 'Confirmed'),
+(3, 5, '2023-12-09', 9, 7, 2, 'Pending'),
+(3, 1, '2023-12-02', 2, 8, 2, 'Confirmed'),
+(3, 2, '2023-12-04', 4, 9, 2, 'Pending'),
+(3, 3, '2023-12-06', 6, 10, NULL, 'Refunded');
 
 
 
@@ -342,9 +326,8 @@ INSERT INTO Booked_Seats (ScreeningID, Seat_ID, Status, BookingID) VALUES
 
 -- NOTIFICATIONS TABLE
 INSERT INTO Notifications (UserID, Message) VALUES
-(1, 'Your booking for "Journey to the Moon" is confirmed.'),
-(2, 'Your booking for "Love in Paris" is pending.'),
-(3, 'Your booking for "The Lost Jungle" is confirmed.'),
-(4, 'New movie "Mystery Manor" is now available.'),
-(5, 'New discount coupons are available.');
+(3, 'Your booking for "Journey to the Moon" is confirmed.'),
+(3, 'Your booking for "Love in Paris" is pending.'),
+(3, 'Your booking for "The Lost Jungle" is confirmed.');
+
 
